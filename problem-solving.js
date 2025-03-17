f97c900d0af096202e69d6c069b6fc59705ebd6b
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-18 02:50:04 +0600

    Problem no 1 - help the zoo manager

--- problem-solving.js
+++ problem-solving.js
@@ -1,1 +1,22 @@
 
+//     Problem-01: Help The Zoo Manager
+
+function calculateMoney(ticketSale) {
+    if( ticketSale < 0 ){
+        return "Baker mama ajke mara khaico, sudhu loss e loss :("
+    }
+
+    let ticketPrice = 120;
+    let securityDailyCost = 500;
+    let staffDailyLunchCost = 8 * 50;
+
+    let dailyTotalWest = securityDailyCost + staffDailyLunchCost;
+    let dailyTotalIncome = ticketSale * ticketPrice;
+
+    let dailyTotalProfit = dailyTotalIncome - dailyTotalWest;
+    return dailyTotalProfit;
+
+}
+
+let todayTicketSale = calculateMoney(-15);
+console.log("Today Baker Mama Profit:", todayTicketSale);
