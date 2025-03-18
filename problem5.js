0a04a099b6441c713bd226f4ad9f297d1529a262
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-18 14:14:04 +0600

    problem 5 solved

--- problem5.js
+++ problem5.js
@@ -85,3 +85,46 @@
 
 // console.log(monthlySavings([5000, 2000, 3500], 7000)); 
 // // Output: 4600
+
+
+
+
+
+
+
+
+
+function monthlySavings(arr, livingCost) {
+
+    if (!Array.isArray(arr) || typeof livingCost !== "number") {
+        return "invalid input";
+    }
+
+    let totalIncome = 0;
+    for (let amount of arr) {
+        if (amount >= 3000) {
+            totalIncome += amount * 0.8; 
+        } else {
+            totalIncome += amount;
+        }
+    }
+
+    let savings = totalIncome - livingCost;
+
+    return savings >= 0 ? savings : "earn more";
+}
+
+console.log(monthlySavings([1000, 2000, 3000], 5400)); 
+// Output: 0
+
+console.log(monthlySavings([1000, 2000, 2500], 5000)); 
+// Output: 500
+
+console.log(monthlySavings([900, 2700, 3400], 10000)); 
+// Output: "earn more"
+
+console.log(monthlySavings(100, [900, 2700, 3400])); 
+// Output: "invalid input"
+
+console.log(monthlySavings([5000, 2000, 3500], 7000)); 
+// Output: 4600
